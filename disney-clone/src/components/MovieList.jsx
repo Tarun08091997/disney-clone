9933d9edd80f31d7066c3d6085d1753b10b32075
Tarun Kumar <tarunsharma080997@gmail.com>
2024-06-02 19:39:57 +0530

    Inal Project

--- disney-clone/src/components/MovieList.jsx
+++ disney-clone/src/components/MovieList.jsx
@@ -1,21 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { getMovieByGenreId } from '../services/ApiServices';
 import MovieCards from './MovieCards';
+import { HiChevronRight } from "react-icons/hi";
+import { HiChevronLeft } from "react-icons/hi";
 
 export default function MovieList({genreID}) {
     const [movieList , setMovieList] = useState([]);
-
+    const scrollRef = useRef(null);
     useEffect(()=>{
         getMovieByGenreId(genreID)
         .then(res => setMovieList(res.data.results))
         .catch(err => console.log(err));
     } , [])
 
+    const scrollLeft = () =>{
+      scrollRef.current.scrollLeft -= window.innerWidth +22
+  }
+
+  const scrollRight = () =>{
+      scrollRef.current.scrollLeft += window.innerWidth - 22
+  }
+
   return (
-    <div className='flex overflow-x-auto scrollbar-hide scroll-smooth gap-3'>
-        {movieList.map((item,index)=>(
-           <MovieCards key={index} movie={item}/>
-        ))}
-    </div>
+    <>
+      <HiChevronLeft className=' text-white text-[40px] absolute mt-[150px] -ml-5 cursor-pointer  hover:scale-150 transition-all duration-100 ease-in-out' onClick={() => scrollLeft()}/>
+      <div ref={scrollRef}  className='flex overflow-x-auto scrollbar-hide scroll-smooth gap-5 p-5'>
+          {movieList.map((item,index)=>(
+            <MovieCards key={index} movie={item}/>
+          ))}
+      </div>
+      <HiChevronRight className=' text-white text-[40px] absolute -mt-[200px] -right-1 cursor-pointer hover:scale-150 transition-all duration-100 ease-in-out' onClick={() => scrollRight()}/>
+    </>
   )
 }
--- disney-clone/src/components/Slider.jsx
+++ disney-clone/src/components/Slider.jsx
@@ -1,48 +1,69 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import { getTrandingMovies } from '../services/ApiServices';
 import { HiChevronRight } from "react-icons/hi";
 import { HiChevronLeft } from "react-icons/hi";
 
-const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original'
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
 
 export default function Slider() {
-
-    const [movieList , setMovieList] = useState([]);
+    const [movieList, setMovieList] = useState([]);
     const scrollRef = useRef(null);
+    const [imageWidth, setImageWidth] = useState(0);
 
-    const getTrandingMovieData = () =>{
+    const getTrandingMovieData = () => {
         getTrandingMovies()
-        .then(res =>setMovieList(res.data.results))
-        .catch(err=>console.log(err))
-    }
+            .then(res => setMovieList(res.data.results))
+            .catch(err => console.log(err));
+    };
 
-    useEffect(()=>{
+    useEffect(() => {
         getTrandingMovieData();
-    },[])
+    }, []);
 
+    useEffect(() => {
+        const updateImageWidth = () => {
+            if (scrollRef.current) {
+                const imageElement = scrollRef.current.querySelector('img');
+                if (imageElement) {
+                    setImageWidth(imageElement.clientWidth);
+                }
+            }
+        };
+        updateImageWidth();
+        window.addEventListener('resize', updateImageWidth);
+        return () => {
+            window.removeEventListener('resize', updateImageWidth);
+        };
+    }, [movieList]);
 
+    const scrollLeft = () => {
+        scrollRef.current.scrollLeft -= imageWidth;
+    };
 
-    const scrollLeft = () =>{
-        scrollRef.current.scrollLeft -= window.innerWidth +22
-    }
+    const scrollRight = () => {
+        scrollRef.current.scrollLeft += imageWidth;
+    };
 
-    const scrollRight = () =>{
-        scrollRef.current.scrollLeft += window.innerWidth - 22
-    }
-
-  return (
-
-    <div>
-        <HiChevronLeft className=' text-white text-[40px] absolute mt-[150px] ml-1 cursor-pointer  hover:scale-150 transition-all duration-100 ease-in-out' onClick={() => scrollLeft()}/>
-        <HiChevronRight className=' text-white text-[40px] absolute mt-[150px] right-1 cursor-pointer hover:scale-150 transition-all duration-100 ease-in-out' onClick={() => scrollRight()}/>
-        <div className='w-full flex overflow-x-auto px-5 py-3 scrollbar-hide scroll-smooth' ref={scrollRef}>
-        {movieList.map((movie , index) =>(
-            <img className='h-[330px] min-w-full 
-              object-left-top object-cover
-              mr-4 rounded-md hover:border-[3px] border-gray-100 transition-all duration-100 ease-in-out'
-             key ={index} src={IMAGE_BASE_URL + movie.backdrop_path} />
-        ))}
+    return (
+        <div className="relative w-full">
+            <HiChevronLeft 
+                className='text-white text-[40px] absolute top-1/2 transform -translate-y-1/2 left-2 cursor-pointer hover:scale-150 transition-all duration-100 ease-in-out z-10' 
+                onClick={scrollLeft} 
+            />
+            <HiChevronRight 
+                className='text-white text-[40px] absolute top-1/2 transform -translate-y-1/2 right-2 cursor-pointer hover:scale-150 transition-all duration-100 ease-in-out z-10' 
+                onClick={scrollRight} 
+            />
+            <div className='w-full flex overflow-x-auto py-3 scrollbar-hide scroll-smooth' ref={scrollRef}>
+                {movieList.map((movie, index) => (
+                    <div key={index} className='flex-shrink-0 w-full pr-4'>
+                        <img 
+                            className='h-[330px] w-full object-left-top object-cover rounded-md hover:border-[3px] border-gray-100 transition-all duration-100 ease-in-out' 
+                            src={IMAGE_BASE_URL + movie.backdrop_path} 
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
-  )
+    );
 }
